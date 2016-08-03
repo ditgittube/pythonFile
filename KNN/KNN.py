23e9353d16b5e8d852e0119c41db49a7ab32fa0e
ditgittube <820738731@qq.com>
2016-08-03 17:23:55 +0800

    upgrade

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -96,14 +96,14 @@ def img2vector(filename):
 
 def handwritingClassTest():
     hwLabels = []
-    trainingFileList = listdir('trainingDigits')                        #导入文件夹得文件名
+    trainingFileList = listdir('trainingDigits')                        #导入文件夹的文件名列表
     m = len(trainingFileList)
     trainingMat = zeros((m, 1024))
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split('.')[0]
         classNumStr = int(fileStr.split('_')[0])
-        hwLables.append(classNumStr)
+        hwLabels.append(classNumStr)
         trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
     testFileList = listdir('testDigits')
     errorCount = 0.0
@@ -113,10 +113,10 @@ def handwritingClassTest():
         fileStr = fileNameStr.split('.')[0]
         classNumStr = int(fileStr.split('_')[0])
         vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
-        classifierResult = classify0(vectorUnder,\
+        classifierResult = classify0(vectorUnderTest,\
                                      trainingMat, hwLabels, 3)
         print ("the classifier came back with: %d, the real answer is: %d "\
-               % (classifierResult, classNumstr))
+               % (classifierResult, classNumStr))
         if (classifierResult != classNumStr): errorCount += 1.0
     print("\nthe total number of errors is: %d" % errorCount)
     print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
